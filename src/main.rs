75cabb578796c4c2006f0f38861c9cdb72a71087
James Bruska <jamesbthecoder@gmail.com>
2022-09-29 15:20:47 -0400

    Added function call to stop thread in Controller and used Box in the enum to reduce size of what is send across channel (it would reserve space for the biggest option if you don't use box, but this comes at a slight speed cost).

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,25 @@ impl Thingy {
   pub fn print_a(&self) { println!("a = {}", self.a.borrow()); }
   //pub fn inc_a(&self, i: i32) { let x = self.a.borrow_mut(); x += i; }
   pub fn inc_a(&self, i: i32) { self.a.replace_with(|&mut old| old + i); }
+  pub fn inc_a_twice(&self, i: i32, j: i32) { self.a.replace_with(|&mut old| old + i + j); }
   pub fn print_hello() { println!("Hello"); }
 }
 
+// JPB: TODO: Tuples cannot be dereferenced into functions
+//            Once they can, change this to use only one box per tuple of Enum arguments
+//            Also can't use "std::ops::Fn::call" because of need to add the Worker reference to args
 enum ThingyFuncs {
   // Internal commands
   ThingyWorkerQuit(),
 
   // Function duplicates
   PrintA(),
-  IncA(i32),
+  IncA(Box<i32>),
+  IncATwice(Box<i32>, Box<i32>),
   PrintHello(),
 }
 
-struct ThingyWorker {}
+struct ThingyWorker;
 impl ThingyWorker {
   pub fn new(i: i32) -> (thread::JoinHandle<()>, ThingyController) {
     let (send, recv) = unbounded::<ThingyFuncs>();
@@ -35,7 +40,8 @@ impl ThingyWorker {
         match recv.recv().unwrap() {
           ThingyFuncs::ThingyWorkerQuit() => break,
           ThingyFuncs::PrintA() => thingy.print_a(),
-          ThingyFuncs::IncA(i) => thingy.inc_a(i),
+          ThingyFuncs::IncA(i) => thingy.inc_a(*i),
+          ThingyFuncs::IncATwice(i, j) => thingy.inc_a_twice(*i, *j),
           ThingyFuncs::PrintHello() => Thingy::print_hello(),
         }
       }
@@ -49,11 +55,20 @@ struct ThingyController {
 }
 
 impl ThingyController {
+  // Internal commands
+  pub fn controller_stop_thread(&self) {
+    self.send.send(ThingyFuncs::ThingyWorkerQuit()).unwrap();
+  }
+
+  // Function duplicates
   pub fn print_a(&self) {
     self.send.send(ThingyFuncs::PrintA()).unwrap();
   }
   pub fn inc_a(&self, i: i32) {
-    self.send.send(ThingyFuncs::IncA(i)).unwrap();
+    self.send.send(ThingyFuncs::IncA(Box::new(i))).unwrap();
+  }
+  pub fn inc_a_twice(&self, i: i32, j: i32) {
+    self.send.send(ThingyFuncs::IncATwice(Box::new(i), Box::new(j))).unwrap();
   }
   pub fn print_hello(&self) {
     self.send.send(ThingyFuncs::PrintHello()).unwrap();
@@ -69,7 +84,9 @@ fn main() {
   thingy.print_a();
   thingy.inc_a(5);
   thingy.print_a();
-  thingy.send.send(ThingyFuncs::ThingyWorkerQuit()).unwrap();
+  thingy.inc_a_twice(1, 5);
+  thingy.print_a();
+  thingy.controller_stop_thread();
 
   handle.join().unwrap();
 }
